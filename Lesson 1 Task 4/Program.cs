6e7fd3c64d66840cf65749a2c12f9344c2c13849
Ольга <olga@mail.ru>
2023-01-17 22:38:43 +0300

    Task4

---
+++ Lesson 1 Task 4/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+System.Console.WriteLine("Введите любое целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+int currentNumber = 1;
+while (currentNumber <= n)
+{
+    if (currentNumber%2 == 0) System.Console.Write(currentNumber + " ");
+    currentNumber++;
+}
