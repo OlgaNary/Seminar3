96cdadb9d87edc224626565f2be90fba12b739e3
Ольга <olga@mail.ru>
2023-01-18 20:40:54 +0300

    Task1

--- Lesson 2 Task 1/Program.cs
+++ Lesson 2 Task 1/Program.cs
@@ -1,3 +1,4 @@
-﻿System.Console.WriteLine("Введите трехзначное число");
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+System.Console.WriteLine("Введите трехзначное число");
 int num = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine($"Вторая цифра числа - {(num/10) % 10}");
