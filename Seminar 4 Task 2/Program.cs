6363c51a9ca56faea42c16b65e21b87cfbf2942d
Ольга <olga@mail.ru>
2023-01-25 22:22:20 +0300

    Task2

---
+++ Seminar 4 Task 2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Программа принимает число и выдает количество цифр в нем.
+System.Console.WriteLine(GetCountNum(new Random().Next(1000, 100000)));
+// Пишем метод
+int GetCountNum(int num)
+{
+    System.Console.WriteLine(num);
+    int count = 0;
+    while (num > 0)
+    {
+        count++;
+        num /= 10;
+    }
+    return count;
+}
