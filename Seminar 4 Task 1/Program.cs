7a9b14bb8e25b4bb9b2c4554536cac2972e8ee24
Ольга <olga@mail.ru>
2023-01-25 22:23:57 +0300

    Task1

---
+++ Seminar 4 Task 1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Программа принимает на вход число А и выдает сумму чтсел от 1 до А
+//7->28
+//4->10
+//8->36
+//int num1 = 7;
+System.Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(GetSum(num1));
+//Пишем метод (функцию)
+int GetSum(int num)
+{
+    int res = 0;
+    for(int i = 1; i <= num; i++)
+    {
+        res+=i;
+    }
+    return res;
+}
+
+
