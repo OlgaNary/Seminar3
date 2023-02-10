2a0b4aab819f8e72e233a642954d8349880e0203
Ольга <olga@mail.ru>
2023-02-10 22:55:44 +0300

    Task2

---
+++ Seminar 8 Task 2/Program.cs
@@ -0,0 +1,47 @@
+﻿//Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+int[,] matrix = Generate2DArray(3, 3);
+Print2DArray(matrix);
+System.Console.WriteLine();
+System.Console.WriteLine();
+Print2DArray(ChangeOrder(matrix));
+
+//Массив заменяет строки на столбцы:
+int[,] ChangeOrder(int[,] matr)
+{
+    int[,] arr = new int[matr.GetLength(0), matr.GetLength(1)];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            arr[i, j] = matr[j, i];
+        }
+    }
+    return arr;
+}
+
+
+int[,] Generate2DArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(1, 100);
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write($"{arr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
