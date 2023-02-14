8a206f6b137b60ce28d76d788f048869a2ed5f97
Ольга <olga@mail.ru>
2023-02-14 11:14:31 +0300

    Task1

---
+++ Seminar 9 Task 1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+System.Console.WriteLine(PrintNumbers(1, 7));
+string PrintNumbers(int start, int end)
+{
+    if (start == end)
+    {
+        return start.ToString();
+    }
+    return (start + " " + PrintNumbers(start + 1, end));
+}
